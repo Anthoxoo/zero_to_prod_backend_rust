bfd3633ba292093a17a06b6f92ba27ab02fb6941
Antoine <briocheguenot@gmail.com>
2026-03-09 00:36:31 +0100

    health check endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpRequest, HttpServer, web};
+
+async fn greet(request: HttpRequest) -> String {
+    let name = request.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("0.0.0.0:8000")?
+    .run()
+    .await
 }
