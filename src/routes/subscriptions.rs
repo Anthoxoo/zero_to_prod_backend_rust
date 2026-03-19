ad7f51b3d0ca02dd5125d4da54c80664bd2c926b
Antoine <briocheguenot@gmail.com>
2026-03-19 15:13:35 +0100

    added a query to keep in track the subscribed user

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
 use actix_web::{HttpResponse, web};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[allow(unused)]
 #[derive(serde::Deserialize)]
@@ -7,9 +10,32 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+    //web::Data<PgConnection> allows us to be understood by actix, actix only understant request within this type : web::Extractor<T> like web::Data<T> web::Form<T> ...
+) -> HttpResponse {
     /*Form here allows us to place informations for us in the FormData struct using the "application/x-www-form-urlencoded" method
      * that looks like this : "name=le%20guin&email=ursula_le_guin%40gmail.com"
      * so the name and email will automaticaly go in the right field */
-    return HttpResponse::Ok().finish(); // TODO!
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => return HttpResponse::Ok().finish(),
+
+        Err(e) => {
+            println!("Error querying the database {e}");
+            return HttpResponse::InternalServerError().finish(); // Allows us to return a error 400 if an error occurs
+        }
+    }
 }
